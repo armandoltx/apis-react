ca758f80cc46f68c4b1ddabac187ae51d46e9e04
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-02 09:08:56 +1000

    agregado un boton

--- projects/random-facts-with-images/src/App.jsx
+++ projects/random-facts-with-images/src/App.jsx
@@ -16,11 +16,6 @@ function App() {
         const data = await response.json()
         const { fact } = data
         setFact(fact)
-
-
-
-
-
       } catch (error) {
         console.log(error)
       }
@@ -35,16 +30,31 @@ function App() {
     const threeFirstWord = fact.split(' ', 3).join (' ')
     console.log(threeFirstWord)
 
-     fetch(`${CAT_PREFIX_IMAGE_URL}/says/${threeFirstWord}?json=true`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        const { _id } = data
-        const url = `${CAT_PREFIX_IMAGE_URL}${_id}/says/${threeFirstWord}?fontSize=50&fontColor=white`
-        setImageUrl(url)
-      })
+    fetch(`${CAT_PREFIX_IMAGE_URL}/says/${threeFirstWord}?json=true`)
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      const { _id } = data
+      const url = `${CAT_PREFIX_IMAGE_URL}${_id}/says/${threeFirstWord}?fontSize=50&fontColor=white`
+      setImageUrl(url)
+    })
   }, [fact])
 
+  const handleClick = () => {
+    const getRandomFact = async () => {
+      try {
+        const response = await fetch(CAT_ENDPOINT_RANDOM_FACT)
+        if(!response.ok) throw new Error('Hubo un error...')
+        const data = await response.json()
+        const { fact } = data
+        setFact(fact)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getRandomFact()
+  }
 
 
 
@@ -52,6 +62,7 @@ function App() {
     <>
       <main>
         <h1>Random Hechos</h1>
+        <button onClick={handleClick}>Create New Fact</button>
         {fact && <p>{fact}</p>}
         {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for {fact}`} />}
       </main>
