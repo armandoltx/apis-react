befac0fe170e83faf011f65218d03017301fba93
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-01 16:05:34 +1000

    llamando a la 2 API; actualmente no da la url asi

--- projects/random-facts-with-images/README.md
+++ projects/random-facts-with-images/README.md
@@ -5,5 +5,5 @@ APIs:
 - Imagen random: <https://cataas.com/cat/says/hello>
 
 - Recupera un hecho aleatorio de gatos de la primera API
-- Recuperar la primera palabra del hecho
+- Recuperar las  3 primeras palabras del hecho
 - Muestra una imagen de un gato con la primera palabra.
--- projects/random-facts-with-images/src/App.jsx
+++ projects/random-facts-with-images/src/App.jsx
@@ -1,18 +1,34 @@
 import {useEffect, useState} from "react"
 
-const URL = "https://catfact.ninja/fact"
+const CAT_ENDPOINT_RANDOM_FACT = "https://catfact.ninja/fact"
 
 function App() {
   const [fact, setFact] = useState()
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     const getRandomFact = async () => {
       try {
-        const response = await fetch(URL)
+        const response = await fetch(CAT_ENDPOINT_RANDOM_FACT)
         if(!response.ok) throw new Error('Hubo un error...')
         const data = await response.json()
         const { fact } = data
         setFact(fact)
+
+        const threeFirstWord = fact.split(' ', 3).join (' ')
+        console.log(threeFirstWord)
+
+        fetch(`https://cataas.com/cat/says/${threeFirstWord}?json=true`)
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            const { _id } = data
+            setImageUrl(`https://cataas.com/cat/${_id}/says/${threeFirstWord}?fontSize=50&fontColor=white`)
+          })
+//             fetch(`https://cataas.com/cat/says/${threeFirstWords}?json=true`)
+//                     setImageUrl(/cat/fqdyCwZeH7IJn7P6/says/hello?fontSize=50&fontColor=white`);
+//  https://cataas.com/cat/fqdyCwZeH7IJn7P6/says/hello?fontSize=50&fontColor=white
+
 
       } catch (error) {
         console.log(error)
@@ -28,7 +44,8 @@ function App() {
   return (
     <>
       <h1>Random Hechos</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for {fact}`} />}
     </>
   )
 }
