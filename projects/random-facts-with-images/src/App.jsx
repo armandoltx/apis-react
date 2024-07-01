374356396ee6564cd6acccd481b671c44f92ba93
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-01 11:00:47 +1000

    llamado a la primera API con fetch

--- projects/random-facts-with-images/src/App.jsx
+++ projects/random-facts-with-images/src/App.jsx
@@ -1,10 +1,32 @@
+import {useEffect, useState} from "react"
 
+const URL = "https://catfact.ninja/fact"
 
 function App() {
+  const [fact, setFact] = useState()
+
+  useEffect(() => {
+    fetch(URL)
+    .then(response => {
+      if(!response.ok) throw Error("Ha habido un error...")
+      console.log(response.json)
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      setFact(data.fact)
+    })
+    .catch(error => console.log(error.message))
+
+  }, [])
+
+
+
 
   return (
     <>
-      <h1> Hola Mundo!</h1>
+      <h1>Random Hechos</h1>
+      <p>{fact}</p>
     </>
   )
 }
