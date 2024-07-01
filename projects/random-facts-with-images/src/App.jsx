d5e5c3ac3fb01050551d5275515bb06213b8d161
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-01 11:06:11 +1000

    Llamado a la primera API con async await

--- projects/random-facts-with-images/src/App.jsx
+++ projects/random-facts-with-images/src/App.jsx
@@ -6,18 +6,14 @@ function App() {
   const [fact, setFact] = useState()
 
   useEffect(() => {
-    fetch(URL)
-    .then(response => {
-      if(!response.ok) throw Error("Ha habido un error...")
-      console.log(response.json)
-      return response.json()
-    })
-    .then(data => {
-      console.log(data)
-      setFact(data.fact)
-    })
-    .catch(error => console.log(error.message))
+    const getRandomFact = async () => {
+      const response = await fetch(URL)
+      const data = await response.json()
+      const { fact } = data
+      setFact(fact)
+    }
 
+    getRandomFact()
   }, [])
 
 
