75adf6b4a93454362aa8d1be29e7f0d3301d0c19
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-03 15:15:00 +1000

    Creado el formulario y agregado el css

--- projects/02-buscador-peliculas/src/App.jsx
+++ projects/02-buscador-peliculas/src/App.jsx
@@ -4,9 +4,19 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      <h1>Buscador de peliculas</h1>
-    </>
+    <div className="page">
+      <header>
+        <h1>Buscador de peliculas</h1>
+
+        <form className='form'>
+          <input type="text" placeholder="Avengers, Star Wars..." />
+          <button type="submit">Buscar</button>
+        </form>
+      </header>
+      <main>
+        Aqui van los resultados de la pelicula con el titulo el ano y la imagen
+      </main>
+    </div>
   )
 }
 export default App
