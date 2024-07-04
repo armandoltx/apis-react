dfef76a635ff13d4303904a2bf5e2ac92768e5ae
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-04 14:49:17 +1000

    agregando un estado para coger el valor introducido en el input, implementando validacion en un useeffect y ensenando errores de validacion

--- projects/02-buscador-peliculas/src/App.jsx
+++ projects/02-buscador-peliculas/src/App.jsx
@@ -1,19 +1,51 @@
-import './App.css'
+import { useState, useEffect } from 'react'
 import { Movies } from './components/Movies'
 import {useMovies} from './hooks/useMovies'
+import './App.css'
 
 function App() {
+  const [query, setQuery] = useState('')
+  const [error, setError] = useState(null)
   const { movies } = useMovies()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({ query })
+  }
+
+  const handleChange = (event) => {
+    setQuery(event.target.value)
+  }
+
+  useEffect(() => {
+    if (query === '') {
+      setError('No se puede buscar una pelicula vacia')
+      return
+    }
+
+    if(query.match(/^\d+$/)) {
+      setError('No se puede buscar una pelicula con un numero')
+      return
+    }
+
+    if(query.length < 3) {
+      setError("Labusqueda tiene que ser mayor de 3 caracteres")
+      return
+    }
+
+    setError(null)
+  },[query])
 
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
 
-        <form className='form'>
-          <input type="text" placeholder="Avengers, Star Wars..." />
+        <form className='form' onSubmit={handleSubmit}>
+          <input onChange={handleChange} value={query} name='query' type="text" placeholder="Avengers, Star Wars..." />
           <button type="submit">Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
       <main>
         <Movies movies={movies} />
