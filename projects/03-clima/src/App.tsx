16a1637e4802bf3f50788f368db50cab50c96467
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-09 11:23:39 +1000

    Creado un componente de formulario

--- projects/03-clima/src/App.tsx
+++ projects/03-clima/src/App.tsx
@@ -1,71 +1,11 @@
-import { ChangeEvent, FormEvent, useState } from "react"
-import type { SearchType } from "./types"
-import { countries } from "../src/data/countries"
+import Form from "./compopnents/Form"
 
 function App() {
-
-  const [search, setSearch] = useState<SearchType>({
-    city: '',
-    country: ''
-  })
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
-    // console.log(event.target.value)
-    setSearch({
-      ...search,
-      [event.target.name]: event.target.value
-    })
-  }
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    //Validamos los campos
-    // console.log(Object.values(search))
-    if(Object.values(search).includes('')) {
-      console.log("Los campos estan vacios...")
-    }
-
-  }
-
 
   return (
     <>
       <h1>Hola Mundo!</h1>
-      <form
-        onSubmit={handleSubmit}
-      >
-        <div>
-          <label htmlFor="city">Ciudad</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            placeholder="Ciudad"
-            value={search.city}
-            onChange={handleChange}
-          />
-        </div>
-        <div>
-          <label htmlFor="country">Pais</label>
-          <select
-            name="country"
-            id="country"
-            value={search.country}
-            onChange={handleChange}
-          >
-            <option value="">-- Seleccione un Pais --</option>
-            {countries.map(country => (
-              <option
-                key={country.code}
-                value={country.code}
-              >
-                {country.name}
-              </option>
-            ))}
-          </select>
-        </div>
-        <input type="submit" value="Consultar Clima" />
-      </form>
+      <Form />
     </>
   )
 }
---
+++ projects/03-clima/src/compopnents/Form.tsx
@@ -0,0 +1,64 @@
+import { ChangeEvent, FormEvent, useState } from "react"
+import type { SearchType } from "../types"
+import { countries } from "../data/countries"
+export default function Form() {
+  const [search, setSearch] = useState<SearchType>({
+    city: '',
+    country: ''
+  })
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
+    // console.log(event.target.value)
+    setSearch({
+      ...search,
+      [event.target.name]: event.target.value
+    })
+  }
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    //Validamos los campos
+    // console.log(Object.values(search))
+    if (Object.values(search).includes('')) {
+      console.log("Los campos estan vacios...")
+    }
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+    >
+      <div>
+        <label htmlFor="city">Ciudad</label>
+        <input
+          type="text"
+          id="city"
+          name="city"
+          placeholder="Ciudad"
+          value={search.city}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="country">Pais</label>
+        <select
+          name="country"
+          id="country"
+          value={search.country}
+          onChange={handleChange}
+        >
+          <option value="">-- Seleccione un Pais --</option>
+          {countries.map(country => (
+            <option
+              key={country.code}
+              value={country.code}
+            >
+              {country.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <input type="submit" value="Consultar Clima" />
+    </form>
+  )
+}
