d07e6a418922761e09637b876c71ddef49d76eb0
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-11 11:48:08 +1000

    creadas las llamadas a la API; agregado zod para crear tipos del resultado de la API; creado .env para variables de entorno

--- projects/03-clima/src/compopnents/Form.tsx
+++ projects/03-clima/src/compopnents/Form.tsx
@@ -1,14 +1,36 @@
 import { ChangeEvent, FormEvent, useState } from "react"
+import { z } from 'zod'
 import type { SearchType } from "../types"
 import { countries } from "../data/countries"
 import Alert from "./Alert"
+
+// schema para zod
+const WeatherSchema = z.object({  // es un objeto pq la API devuelve un objeto -> {coord: {…}, weather: Array(1), base: 'stations', main: {…}, visibility: 10000, …}
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_min: z.number(),
+    temp_max: z.number()
+  })
+})
+
+type Weather = z.infer<typeof WeatherSchema>
+
 export default function Form() {
   const [search, setSearch] = useState<SearchType>({
     city: '',
     country: ''
   })
 
-  const [error, setError] =useState('')
+  const [error, setError] = useState('')
+  const [weather, setWeather] = useState<Weather>({
+    name: '',
+    main: {
+      temp: 0,
+      temp_min: 0,
+      temp_max: 0
+    }
+  })
 
   const handleChange = (event: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLSelectElement>) => {
     // console.log(event.target.value)
@@ -18,16 +40,55 @@ export default function Form() {
     })
   }
 
+  const fetchWeather = async () => {
+    const apiKey = import.meta.env.VITE_API_KEY
+    console.log("llamando a la API")
+
+    try {
+      const geoResponse = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${apiKey}`)
+      // console.log(geoResponse)
+      if (!geoResponse.ok) throw new Error('Hubo un error con la API...')
+
+      const geoData = await geoResponse.json()
+      console.log(geoData)
+      const lat = geoData[0].lat
+      const lon = geoData[0].lon
+      console.log(lat, lon)
+
+      if ([lat, lon].includes('')) throw new Error('Latitud o Longitud no existen')
+
+      const weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+
+      const weatherData = await weatherResponse.json()
+      // console.log(weatherData)
+      const result = WeatherSchema.safeParse(weatherData)
+      // console.log(result)
+      if(result.success) {
+        console.log(result.data.name)
+        setWeather(result.data)
+      }
+
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     //Validamos los campos
-    // console.log(Object.values(search))
+
     if (Object.values(search).includes('')) {
       // console.log("Los campos estan vacios...")
       setError('Todos los campos son obligatorios')
       return
     }
+
+    // llamamos a la API
+    fetchWeather(search)
   }
+
+
 
   return (
     <form
