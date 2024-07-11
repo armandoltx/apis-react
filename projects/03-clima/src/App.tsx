aa7d05258fa9287d809e3bbde50f3c0ab5d8e06a
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-11 16:05:56 +1000

    Creado el lado donde se muestran los resultados y aplicarle estilos

--- projects/03-clima/src/App.tsx
+++ projects/03-clima/src/App.tsx
@@ -1,11 +1,27 @@
+import './App.css'
 import Form from "./compopnents/Form"
 
 function App() {
 
   return (
     <>
-      <h1>Hola Mundo!</h1>
-      <Form />
+      <div className="wrapper">
+        <div className="header">
+        <h1>Buscador de Clima</h1>
+        </div>
+        <div className="main">
+          <Form />
+
+          <div className="results">
+            <h2>Clima de: Cadiz</h2>
+            <p className="current">Temperatura Actual: 3333&deg;</p>
+            <div className="min-max-temp">
+              <p>Min: 12&deg;</p>
+              <p>Max: 50&deg;</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
