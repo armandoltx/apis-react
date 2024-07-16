84126f73daf445ae15cc3dee43710e18c1677f95
Armando Ruiz <armando.ruiz@industrie.co>
2024-07-16 14:37:09 +1000

    creado el formulario

--- projects/04-criptomonedas-typescript/src/App.tsx
+++ projects/04-criptomonedas-typescript/src/App.tsx
@@ -6,6 +6,28 @@ function App() {
   return (
     <>
       <h1>Criptomonedas</h1>
+      <form>
+        <div>
+          <label htmlFor="currency">Moneda</label>
+          <select
+            name="currency"
+            id="currency"
+          >
+              <option value="">-- Selecciona una Moneda --</option>
+            </select>
+        </div>
+        <div>
+          <label htmlFor="cripto-currency">Criptomoneda</label>
+          <select
+            name="cripto-currency"
+            id="cripto-currency"
+          >
+            <option value="">-- Selecciona una Criptomoneda --</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Cotizar" />
+      </form>
     </>
   )
 }
