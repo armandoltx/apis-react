6fcadf668e19538c662fe7751cd0465f612191e6
Armando Ruiz <armando.ruiz@industrie.co>
2024-09-16 13:11:55 +1000

    agregado los productos y listado cada producto

--- projects/05-shopping-cart/src/App.jsx
+++ projects/05-shopping-cart/src/App.jsx
@@ -1,10 +1,15 @@
 
+import { products as initialProducts } from './mocks/products.json'
+import { Products } from './components/Products.jsx'
+import {useState} from 'react'
 
 function App() {
+  const [products] = useState(initialProducts)
 
   return (
     <>
-      Shopping Cart
+      <h1>Shopping Cart</h1>
+      <Products products={products}/>
     </>
   )
 }
---
+++ projects/05-shopping-cart/src/components/Products.jsx
@@ -0,0 +1,23 @@
+import './Products.css'
+import { AddToCartIcon } from './Icons'
+
+export function Products ({ products }) {
+  return(
+    <main className="products">
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>
+            <img
+              src={product.thumbnail}
+              alt={product.description}
+            />
+            <div>
+              <strong>{product.title}</strong> - ${product.price}
+            </div>
+            <button><AddToCartIcon /></button>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
+}
