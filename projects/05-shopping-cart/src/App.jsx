0324a763509e5192cce93d925ac8778b2b551964
Armando Ruiz <armando.ruiz@industrie.co>
2024-09-17 11:37:49 +1000

    Creado los distintos componentes para la app y creado el Context y el hook para usar el context de los filtros

--- projects/05-shopping-cart/src/App.jsx
+++ projects/05-shopping-cart/src/App.jsx
@@ -2,14 +2,20 @@
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from './components/Products.jsx'
 import {useState} from 'react'
+import {Header} from './components/Header.jsx'
+import { Footer } from './components/Footer.jsx'
+import { useFilters } from './hooks/useFilters.js'
 
 function App() {
   const [products] = useState(initialProducts)
+  const {filterProducts} = useFilters()
+  const filteredProducts = filterProducts(products)
 
   return (
     <>
-      <h1>Shopping Cart</h1>
-      <Products products={products}/>
+      <Header/>
+      <Products products={filteredProducts}/>
+      <Footer />
     </>
   )
 }
