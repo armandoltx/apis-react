2333d28eca43874ef07a6f49a6b460ee74e06a60
Armando Ruiz <armando.ruiz@industrie.co>
2024-09-17 15:14:17 +1000

    Agregado la funcionalidad del carrito

--- projects/05-shopping-cart/src/components/Cart.jsx
+++ projects/05-shopping-cart/src/components/Cart.jsx
@@ -1,10 +1,30 @@
 import {useId} from "react"
 import { CartIcon, ClearCartIcon } from "./Icons"
 import './Cart.css'
+import {useCart} from "../hooks/useCart"
+
+function CartItem({ thumbnail, price, title, quantity, addToCart }) {
+  return(
+    <li>
+      <img
+        src={thumbnail}
+        alt={title}
+      />
+      <div>
+        <strong>{title}</strong> - ${price}
+      </div>
+      <footer>
+        <small>Qty: {quantity}</small>
+        <button onClick={addToCart}>+</button>
+      </footer>
+    </li>
+  )
+}
 
 
-export function Cart () {
+export function Cart() {
   const cartCheckboxId = useId()
+  const {cart, clearCart, addToCart} = useCart()
 
   return(
     <>
@@ -14,21 +34,16 @@ export function Cart () {
     <input type="checkbox" id={cartCheckboxId} hidden />
     <aside className="cart">
       <ul>
-        <li>
-          <img
-            src="https://i.dummyjson.com/data/products/30/thumbnail.jpg"
-            alt="Iphone"
+        {cart.map(product => (
+          <CartItem
+            key={product.id}
+            {...product}
+            addToCart={() => addToCart(product)}
           />
-          <div>
-            <strong>iPhone</strong> - $1499
-          </div>
-          <footer>
-            <small>Qty: 1</small>
-            <button>+</button>
-          </footer>
-        </li>
+        ))}
+
       </ul>
-      <button>
+      <button onClick={clearCart}>
         <ClearCartIcon />
       </button>
     </aside>
